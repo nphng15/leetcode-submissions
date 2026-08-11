c92ef8e67e7ec3b766bccf6fe43f805ed9270db5
Fongg <46365986+nphng15@users.noreply.github.com>
2026-08-11 23:18:29 +0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max=0;
+        for (int i : piles){
+            if (i>max){
+                max = i;
+            }
+        }
+
+        int rs=-1;
+        int left = 1;
+        int right = max;
+
+        while (left<=right){
+            int mid = (left+right)/2;
+            System.out.println("MID:" + mid);
+
+            int sum=0;
+            for (int i : piles){
+                sum+= (int) Math.ceil((double) i/mid);
+            }
+            System.out.println("SUM:" + sum);
+
+            if (sum<=h){ 
+                rs = mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }    
+        }
+
+        return rs;
+    }
+}
