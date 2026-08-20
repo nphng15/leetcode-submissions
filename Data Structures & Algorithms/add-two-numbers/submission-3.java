46950ba94f7dc0f21cb27d4520f3bbb3324240f5
Fongg <46365986+nphng15@users.noreply.github.com>
2026-08-20 14:57:30 +0700

    Add: add-two-numbers - submission-3

---
+++ Data Structures & Algorithms/add-two-numbers/submission-3.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int check=0;
+        ListNode result = new ListNode(-1);
+        ListNode curr = result;
+        while (l1 != null && l2 != null){
+            int tmp = l1.val + l2.val + check;
+            if (tmp > 9){
+                tmp = tmp % 10;
+                curr.next = new ListNode(tmp);
+                check=1;
+            }
+            else {
+                curr.next = new ListNode(tmp);
+                check=0;
+            }
+            curr = curr.next;
+            l1=l1.next;
+            l2=l2.next;
+        }
+
+        while (l1!=null){
+            int tmp = l1.val + check;
+            if (tmp > 9){
+                tmp = tmp % 10;
+                curr.next = new ListNode(tmp);
+                check=1;
+            }
+            else {
+                curr.next = new ListNode(tmp);
+                check=0;
+            }
+            curr=curr.next;
+            l1 = l1.next;
+        }
+        
+        while (l2!=null){
+            int tmp = l2.val + check;
+            if (tmp > 9){
+                tmp = tmp % 10;
+                curr.next = new ListNode(tmp);
+                check=1;
+            }
+            else {
+                curr.next = new ListNode(tmp);
+                check=0;
+            }
+            curr=curr.next;
+            l2 = l2.next;
+        }
+
+        if (check==1){
+            curr.next = new ListNode(check);
+        }
+
+        return result.next;
+    }
+}
